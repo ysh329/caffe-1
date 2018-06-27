798530073e9c4d571b4ea540fd2b515d0842c109
Yuan Shuai <ysh329@sina.com>
2018-06-27 11:17:23 +0000

    add mtcnn example

---
+++ detect_face_image_using_mtcnn_det1.sh
@@ -0,0 +1,6 @@
+./build/examples/cpp_classification/classification.bin \
+  examples/MTmodel/det1.prototxt \
+  examples/MTmodel/det1.caffemodel \
+  data/ilsvrc12/imagenet_mean.binaryproto \
+  data/ilsvrc12/synset_words_1.txt \
+  examples/images/cat.jpg
---
+++ detect_face_image_using_zb_det.sh
@@ -0,0 +1,6 @@
+./build/examples/cpp_classification/classification.bin \
+  models/zb_det/face_det.prototxt \
+  models/zb_det/face_det.caffemodel \
+  data/ilsvrc12/imagenet_mean.binaryproto \
+  data/ilsvrc12/synset_words_1.txt \
+  examples/images/cat.jpg
