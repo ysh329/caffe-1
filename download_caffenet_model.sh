0c896c6c9fc7df1fe853ee257c63537b5dff3db1
Yuan Shuai <ysh329@sina.com>
2018-06-27 03:11:25 +0000

    add scripts

---
+++ download_caffenet_model.sh
@@ -0,0 +1,5 @@
+# download caffemodel and prototxt
+./scripts/download_model_binary.py models/bvlc_reference_caffenet
+
+# download imagenet mean file
+./data/ilsvrc12/get_ilsvrc_aux.sh .
---
+++ run_caffenet_forward_gpu.sh
@@ -0,0 +1,6 @@
+./build/tools/caffe time \
+-model=models/bvlc_reference_caffenet/deploy.prototxt \
+-weights=./models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
+-iterations=1 \
+-gpu=0
+
